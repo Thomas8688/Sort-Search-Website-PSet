a72ddc05267382185505765c3b6897f3c89a8e9e
Thomas Eley <thomas-eley@hotmail.co.uk>
2020-10-28 23:06:09 +0400

    Bubble Sort Test Scenarios Working - Minor Styling

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for, request
+from random import randint
+from os import path
 
 def bubbleSort(lst):
     lst = [int(x) for x in lst]
@@ -16,29 +18,83 @@ def validate(lst):
             int(item)
         except:
             valid = False
-    if valid:
-        return True
-    else:
-        return False
+    return valid
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html", sorted = "HOMEPAGE")
+    return render_template("index.html")
 
 
-@app.route('/bubble', methods=['POST', 'GET'])
-def bubble():
+@app.route('/bubble/enter', methods=['POST', 'GET'])
+def bubbleenter():
     if request.method == 'POST':
         task_content = request.form['numbers']
-        task_content = task_content.split(",")
-        while task_content[-1] in [",", ""]:
-            task_content.pop(-1)
-        if validate(task_content):
-            task_content = bubbleSort(task_content)
-            return render_template("bubble.html", sorted = task_content)
+        if task_content:
+            task_content = task_content.split(",")
+            while task_content[-1] in [",", ""]:
+                task_content.pop(-1)
+            if validate(task_content):
+                task_content = bubbleSort(task_content)
+                return render_template("bubbleenter.html", sorted = task_content)
+            else:
+                return render_template("bubbleenter.html", sorted = [])
         else:
-            return render_template("bubble.html", sorted = ['Invalid Input'])
+            return render_template("bubbleenter.html", sorted = [])
     else:
-        return render_template("bubble.html", sorted = [])
+        return render_template("bubbleenter.html", sorted = [])
+
+
+@app.route('/bubble/generate', methods=['POST', 'GET'])
+def bubblegnerate():
+    if request.method == 'POST':
+        amount = request.form['amount']
+        rnge = request.form['range']
+        if amount and rnge:
+            try:
+                num = int(amount)
+                rnge = rnge.split(",")
+                min = int(rnge[0])
+                max = int(rnge[1])
+                if len(rnge) == 2 and validate(rnge) and min < max:
+                    if num > 0 and num < 101:
+                        lst = []
+                        for i in range(num):
+                            lst.append(randint(min, max))
+                        srt = bubbleSort(lst)
+                        return render_template("bubblegenerate.html", sort = srt, genlst = lst)
+                    else:
+                        return render_template("bubblegenerate.html", sort = [], genlst = ['Invalid Amount'])
+                else:
+                    return render_template("bubblegenerate.html", sort = [], genlst = ['Invalid Range'])
+            except:
+                return render_template("bubblegenerate.html", sort = [], genlst = ['Invalid Amount'])
+        else:
+                return render_template("bubblegenerate.html", sort = [], genlst = [])
+    else:
+        return render_template("bubblegenerate.html", sort = [], genlst = [])
+
+
+@app.route('/bubble/file', methods=['POST', 'GET'])
+def bubblefile():
+    if request.method == 'POST':
+        filecont = request.form['filepath']
+        type = filecont.split(".")
+        if type[-1] == "csv":
+            if path.exists(filecont):
+                with open(filecont, 'r') as file:
+                    data = file.read().replace('\n', '')
+                data = data.split(",")
+                if validate(data):
+                    for i in range(len(data)):
+                        data[i] = int(data[i])
+                    datasrt = bubbleSort(data)
+                    return render_template("bubblefile.html", cont=data, sort=datasrt)
+                else:
+                    return render_template("bubblefile.html", cont=['Invalid Data'], sort = [])
+            else:
+                return render_template("bubblefile.html", cont=['Invalid Path'], sort = [])
+        else:
+            return render_template("bubblefile.html", cont=['File must be .csv'], sort = [])
+    return render_template("bubblefile.html", cont=[], sort = [])
